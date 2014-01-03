db8e34fc6b62c5e4e0104c39d86ab6760b9e6497
Laurie <laurie.walroth@gmail.com>
2014-01-03 08:37:03 -0800

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 DentalHygiene::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 
   root 'static_pages#home'
   match '/signup',    to: 'users#new',    via: 'get'
+  match '/signin',    to: 'sessions#new', via: 'get'
+  match '/signout',   to: 'sessions#destroy', via: 'delete'
   match '/services',    to: 'static_pages#services',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
